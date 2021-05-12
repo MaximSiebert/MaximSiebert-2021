21329f729e21eb93d6d477417f7d6b742b16ba2d
Maxim Siebert <maxim.siebert@gmail.com>
2021-05-12 16:50:31 -0400

    footer component

---
+++ src/components/Footer.svelte
@@ -0,0 +1,45 @@
+<script>
+	var time = '';
+	var year = '';
+
+	function updateClock() {
+		var now = new Date(), // current date
+			hour = now.getHours(),
+			minutes = now.getMinutes(),
+			sep = ':',
+			ampm = 'am';
+		
+		if (hour >= 12) {
+				hour -= 12;
+				ampm = 'pm';
+		}
+				
+		minutes = minutes < 10 ? '0' + minutes : minutes;
+		hour = hour % 12;
+		hour = hour ? hour : 12; // the hour '0' should be '12'
+
+		// set the content of the element with the ID time to the formatted string
+		time = hour + sep + minutes + ' ' + ampm;
+		year = now.getFullYear();
+
+		// call this function again in 1000ms
+		setTimeout(updateClock, 1000);
+
+	}
+
+	updateClock();
+</script>
+
+<div class="flex pt-5 mt-auto text-gray-600 sm:pt-8 md:pt-12 dark:text-gray-400">
+    <div class="w-8/12 px-4 sm:w-3/12">
+        <p>Ottawa – {time} ET</p>
+    </div>
+    <div class="flex w-4/12 ml-auto sm:w-8/12">
+        <div class="hidden w-8/12 px-4 sm:block">
+            <p>Updated 05/2021</p>
+        </div>
+        <div class="flex justify-end w-full px-4 sm:w-4/12">
+            <p>2011 – {year} ©</p>
+        </div>
+    </div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -22,43 +22,15 @@
 </script>
 
 <script>
+	import Footer from '../components/Footer.svelte';
+	
 	export let projects;
 	export let experiences;
 	export let collaborators;
 	export let services;
 	export let links;
 
-	var time = '';
-	var year = '';
-
 	let description = "is a French Canadian designer & developer. He's currently a Principal Designer at <a href=\"https://blunt.af/\" class=\"hover:text-black dark:hover:text-white hover:underline\" target=\"_blank\" rel=\"noreferrer noopener\">Peggy</a> and freelances through his studio <a href=\"https://blunt.af/\" class=\"hover:text-black dark:hover:text-white hover:underline\" target=\"_blank\" rel=\"noreferrer noopener\">Blunt</a>. This site lists most of the work he's done over the years, if you'd like to discuss new commissions or collaborate with Maxim email him at <a href=\"mailto:maxim@hey.com\" class=\"hover:text-black dark:hover:text-white hover:underline\">maxim@hey.com</a>."
-	
-	function updateClock() {
-		var now = new Date(), // current date
-			hour = now.getHours(),
-			minutes = now.getMinutes(),
-			sep = ':',
-			ampm = 'am';
-		
-		if (hour >= 12) {
-				hour -= 12;
-				ampm = 'pm';
-		}
-				
-		minutes = minutes < 10 ? '0' + minutes : minutes;
-		hour = hour % 12;
-		hour = hour ? hour : 12; // the hour '0' should be '12'
-
-		// set the content of the element with the ID time to the formatted string
-		time = hour + sep + minutes + ' ' + ampm;
-		year = now.getFullYear();
-
-		// call this function again in 1000ms
-		setTimeout(updateClock, 1000);
-
-	}
-
-	updateClock();
 </script>
 
 <svelte:head>
@@ -156,17 +128,5 @@
 			{/each}
 		</div>
 	</section>
-	<div class="flex pt-5 mt-auto text-gray-600 sm:pt-8 md:pt-12 dark:text-gray-400">
-		<div class="w-8/12 px-4 sm:w-3/12">
-			<p>Ottawa – {time} ET</p>
-		</div>
-		<div class="flex w-4/12 ml-auto sm:w-8/12">
-			<div class="hidden w-8/12 px-4 sm:block">
-				<p>Updated 05/2021</p>
-			</div>
-			<div class="flex justify-end w-full px-4 sm:w-4/12">
-				<p>2011 – {year} ©</p>
-			</div>
-		</div>
-	</div>
+	<Footer />
 </div>
